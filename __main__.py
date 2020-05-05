33efc89d16e4679a1d633cf0940df8950830a845
FelixMo42 <felix@tacocat.com>
2020-05-04 22:45:38 -0700

    added ziping and improved image quality

--- __main__.py
+++ __main__.py
@@ -6,9 +6,9 @@ SidePadding = 30
 PageWidth = 612
 PageHeight = 1008
 
-FontSize = 10
+FontSize = 8
 
-Scale = 0.7
+Scale = 0.64
 
 ## END SETTINGS ##
 
@@ -18,13 +18,11 @@ import glob
 import csv
 import shutil
 import os
-from random import shuffle
-from PIL import Image, ImageOps, ImageFilter
+import zipfile
+import random
+from PIL import Image, ImageOps
 
 def exportTest(out="."):
-    if not os.path.exists(out):
-        os.makedirs(out)
-
     def addQuestion(src):
         nonlocal questionCounter
         nonlocal position
@@ -35,8 +33,8 @@ def exportTest(out="."):
         # load the image
         pix = fitz.Pixmap(src)
 
-        width = pix.width
-        height = pix.height
+        width = pix.width * Scale
+        height = pix.height * Scale
 
         # is their enough space left on this page for the question?
         if (position + height > PageHeight - TopPadding):
@@ -59,13 +57,20 @@ def exportTest(out="."):
         page.insertText(fitz.Point(X0 + 10, Y0 + FontSize + 10), str(questionCounter), fontsize=FontSize)
 
         # insert the picture of the multiple choice question
-        page.insertImage(fitz.Rect(X0 + 50, Y0 + 10, X0 + 50 + width, Y1 - 10), pixmap=pix)
+        page.insertImage(fitz.Rect(
+            X0 + 50,         Y0 + 10,
+            X0 + 50 + width, Y0 + 10 + height
+        ), pixmap=pix)
 
         # draw the rectang around the question
         page.drawRect( fitz.Rect(X0, Y0, X1, Y1) )
 
         # shift down the position to the end of this question
         position = Y1
+
+    # creat the output directory if it dosent exist
+    if not os.path.exists(out):
+        os.makedirs(out)
 
     # how many question we have
     questionCounter = 0
@@ -98,18 +103,18 @@ def exportTest(out="."):
             i += 1
 
 def getAllInFolder(src, ext):
+    # get the list of files
     files = os.listdir(src)
 
+    # return a filter list that end in the right extention
     return filter(lambda path: path.endswith(ext) or path.endswith(ext.upper()), files)
 
 def trimWhiteSpaceFromImages(src, out):
-    # remove the output folder if it exist
-    if os.path.isdir(out):
-        os.rmdir(out)
-    
-    # then create a new fresh empty one
-    os.mkdir(out)
+    # make sure an output directory exist
+    if not os.path.isdir(out):
+        os.mkdir(out)
 
+    # trim and scale all the images
     for path in getAllInFolder("data", ".png"):
         # load the image
         image = Image.open(f"{src}/{path}")
@@ -124,9 +129,6 @@ def trimWhiteSpaceFromImages(src, out):
 
         # crop the image
         image = image.crop(imageBox)
-
-        # resize it
-        image = image.resize( (int(image.width * Scale), int(image.height * Scale)), Image.ANTIALIAS )
 
         # save it
         image.save(f"{out}/{path}")
@@ -144,11 +146,23 @@ def loadRandomizedQuestions(src, sheet="Sheet1"):
         questions.append( sheet.row_values(rownum) )
 
     # shuffle the list of questions
-    shuffle(questions)
+    random.shuffle(questions)
 
     # and finally awnser
     return questions
 
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
 # trimWhiteSpaceFromImages("data", "tmp")
 for i in range(0, 15):
-    exportTest(f"out/{i}")
+    exportTest(f"out/{20 + i}")
+
+# zip the output directory
+zipf = zipfile.ZipFile('out.zip', 'w', zipfile.ZIP_DEFLATED)
+zipdir('out/', zipf)
+zipf.close()
+
